d0f45df9ad94e3e076dcf4d3dd2b98e6d1e9b081
vakungur <vakungur@gmail.com>
2020-08-20 13:16:00 +0300

    The project was initialized.

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+import './index.scss';
+
+UIkit.use(Icons);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // eslint-disable-next-line no-undef
+  document.getElementById('root'),
+);
+
+serviceWorker.unregister();
